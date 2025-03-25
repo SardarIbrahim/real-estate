43352096b20f56abad0b34ec1da6ad08711793ef
Sardar Ibrahim <KhanzadaIbrahim05@gmail.com>
2025-03-25 07:02:12 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import express from 'express'
 
+import { config } from 'dotenv'
+
+config()
+
+import mongoose from 'mongoose'
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    dbName: 'real-estate',
+  })
+  .then(() => console.log('DB IS UP'))
+  .catch((err) => console.log('DB ERROR ', err))
+
 const app = express()
 
 app.listen(3000, () => {
